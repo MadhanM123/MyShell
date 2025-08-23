f531e7bb3db6c54d3a2b5ee6544d6a2ddf164bfd
MadhanM123 <madhangopalvel@gmail.com>
2025-08-23 12:07:02 -0700

    read_line done

--- src/myshell.c
+++ src/myshell.c
@@ -2,6 +2,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define SHELL_BUFSZ 1024
+char* shell_read_line(void){
+    int bufsz = SHELL_BUFSZ;
+    int pos = 0;
+    char* buf = malloc(sizeof(char) * bufsz);
+    int c;
+
+    if(!buf){
+        fprintf(stderr, "shell: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1){
+        c = getchar();
+
+        if(c == EOF || c == '\n'){
+            buf[pos] = '\0';
+            return buf;
+        }
+        else{
+            buf[pos] = c;
+        }
+        pos++;
+
+        if(pos >= bufsz){
+            bufsz += SHELL_BUFSZ;
+            buf = realloc(buf, bufsz);
+            if(!buf){
+                fprintf(stderr, "shell: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
 
 void shell_loop(void){
     char* line;
