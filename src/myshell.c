32c5b04e1b1fbe1e7186184fc81de9916040ef37
MadhanM123 <madhangopalvel@gmail.com>
2025-08-24 12:38:46 -0700

    Added builtin shell commands

--- src/myshell.c
+++ src/myshell.c
@@ -3,6 +3,59 @@
 #include <stdio.h>
 #include <sys/wait.h>
 
+// Builtin Shell Command Forward Declarations
+
+int shell_cd(char** args);
+int shell_help(char** args);
+int shell_exit(char** args);
+
+// Builtin Shell Command Names
+
+char* builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+int (*builtin_func[]) (char**) = {
+    &shell_cd,
+    &shell_help,
+    &shell_exit
+};
+
+int shell_num_builtins(){
+    return sizeof(builtin_str) / sizeof(char*);
+}
+
+int shell_cd(char** args){
+    if(args[1] == NULL){
+        fprintf(stderr, "shell: expected argument to cd\n");
+    }
+    else{
+        if(chdir(args[1]) != 0){
+            perror("shell");
+        }
+    }
+    return 1;
+}
+
+int shell_help(char** args){
+    printf("Madhan's Shell\n");
+    printf("Type program names and arguments, and enter.\n");
+    printf("The following are built in:\n");
+
+    for(int i = 0; i < shell_num_builtins(); i++){
+        printf(" %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command to learn information about other programs.\n");
+    return 1;
+}
+
+int shell_exit(char** args){
+    return 0;
+}
+
 
 int shell_launch(char** args){
     __pid_t pid, wpid;
@@ -11,12 +64,12 @@ int shell_launch(char** args){
     pid = fork();
     if(pid == 0){
         if(execvp(args[0], args) == -1){
-            perror("shell: error");
+            perror("shell");
         }
         exit(EXIT_FAILURE);
     }
     else if(pid < 0){
-        perror("shell: forking error");
+        perror("shell");
     }
     else{
         do{
